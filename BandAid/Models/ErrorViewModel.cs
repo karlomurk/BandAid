2b4661e9bf9e81ff960d57c9e72f15804c171f85
Robert Horvat <robbie_horvat@hotmail.com>
2019-04-29 21:23:16 +0200

    Add project files.

---
+++ BandAid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BandAid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
